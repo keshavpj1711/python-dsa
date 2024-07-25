41f8109a73779bd150b749ba85042dfa4d7dead2
Keshav Prajapati <keshavpj1711@gmail.com>
2024-07-25 21:55:35 +0530

    test cases for bs

---
+++ BS-LinkedLists-Complexity/binarySearch/bs.py
@@ -0,0 +1,24 @@
+# We need to write a program to find the position of a given number in a list of numbers arranged in decreasing order.
+# We also need to minimize the number of times we access elements from the list.
+
+# Listing the test cases
+# - The element is found
+# - The element is not found
+# - The list is empty
+# - The list contains multiple instances of target
+
+test = [
+    {'input': {'cards': [13, 11, 10, 7, 4, 3, 1, 0], 'query': 7}, 'output': 3},
+    {'input': {'cards': [13, 11, 10, 7, 4, 3, 1, 0], 'query': 1}, 'output': 6},
+    {'input': {'cards': [4, 2, 1, -1], 'query': 4}, 'output': 0},
+    {'input': {'cards': [3, -1, -9, -127], 'query': -127}, 'output': 3},
+    {'input': {'cards': [6], 'query': 6}, 'output': 0},
+    {'input': {'cards': [9, 7, 5, 2, -9], 'query': 4}, 'output': -1},
+    {'input': {'cards': [], 'query': 7}, 'output': -1},
+    {'input': {'cards': [8, 8, 6, 6, 6, 6, 6, 3, 2, 2, 2, 0, 0, 0], 'query': 3}, 'output': 7},
+    {'input': {'cards': [8, 8, 6, 6, 6, 6, 6, 6, 3, 2, 2, 2, 0, 0, 0], 'query': 6}, 'output': 2}
+]
+
+
+def locate_number(list, target):
+  pass
